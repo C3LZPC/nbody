c21c50e2b3f85c2d41045c79f616b4f695009627
C3LZPC <c3lzpc@inf.elte.hu>
2024-05-05 18:28:21 +0200

    Added various point and acceleration setup options

--- gui.gd
+++ gui.gd
@@ -5,9 +5,20 @@ func num_points(num : int) -> void:
 	$Sim_Settings/Sim_Settings/Particle_Count_Label.text = "Particles: " + str(num)
 
 func _ready():
-	var distributions = ["Uniform"]
+	var distributions = []
+	distributions.append("Uniform: Box")
+	distributions.append("Non-Uniform: Sphere")
+	distributions.append("Uniform: Sphere")
+	distributions.append("Non-Uniform: 3 Sphere")
 	for i in distributions:
 		$Sim_Settings/Sim_Settings/Distribution.add_item(i)
+	
+	var accelerations = []
+	accelerations.append("None")
+	accelerations.append("x0.1")
+	accelerations.append("x1.0")
+	for i in accelerations:
+		$Sim_Settings/Sim_Settings/Initial_Acceleration.add_item(i)
 
 func _process(delta):
 	var fps = Engine.get_frames_per_second()
--- main.gd
+++ main.gd
@@ -4,43 +4,91 @@ extends Node3D
 const collision_treshhold = 1e-3
 
 
-
 var G = 6.674e-11 # https://en.wikipedia.org/wiki/Gravitational_constant
 
 var mesh = ArrayMesh.new()
 
-#var points = PackedVector3Array()
 var points = []
 var masses = PackedFloat64Array()
-#var accelerations = PackedVector3Array()
 var accelerations = []
 
 func _ready():
+	seed(123434423443524)
 	$GUI/Sim_Settings/Sim_Settings/Reset.connect("pressed", reset_sim)
 	$GUI/Sim_Settings/Sim_Settings/G.value = G
 	
 	$MeshInstance3D.mesh = mesh
 
 func reset_sim():
+	$Camera3D.reset_camera()
 	G = $GUI/Sim_Settings/Sim_Settings/G.value
 	var particle_num = $GUI/Sim_Settings/Sim_Settings/Particle_Count.value
+	var distribution_option = $GUI/Sim_Settings/Sim_Settings/Distribution.get_selected_id()
+	var accel_option = $GUI/Sim_Settings/Sim_Settings/Initial_Acceleration.get_selected_id()
 	
 	var sim_radius = 10
 	var starting_mass = 1000.0
 	
-	#points = PackedVector3Array()
 	points = []
 	masses = PackedFloat64Array()
-	#accelerations = PackedVector3Array()
 	accelerations = []
 	
 	
 	for x in particle_num:
-		#var p = Vector3(sim_radius * randf() - (sim_radius / 2.0), 0, sim_radius * randf() - (sim_radius / 2.0))
-		var p = [sim_radius * randf() - (sim_radius / 2.0), 0.0, sim_radius * randf() - (sim_radius / 2.0)]
+		var p = [0.0, 0.0, 0.0]
+		
+		if distribution_option == 0:
+			#print("Uniform distribution: Box")
+			p[0] = sim_radius * randf() - (sim_radius / 2.0)
+			p[1] = sim_radius * randf() - (sim_radius / 2.0)
+			p[2] = sim_radius * randf() - (sim_radius / 2.0)
+		
+		if distribution_option == 1:
+			#print("Non-Uniform distribution: Sphere")
+			var r = sim_radius * randf()
+			var theta = randf() * 2.0 * PI
+			var phi = randf() * PI
+			p[0] = r * sin(phi) * cos(theta)
+			p[1] = r * sin(phi) * sin(theta)
+			p[2] = r * cos(phi)
+		
+		if distribution_option == 2:
+			#print("Uniform distribution: Sphere")
+			var r = sim_radius * randf()
+			p[0] = (randf() - 0.5) * r
+			p[1] = (randf() - 0.5) * r
+			p[2] = (randf() - 0.5) * r
+		
+		if distribution_option == 3:
+			#print("Non-Uniform distribution: 3 Sphere")
+			var r = sim_radius * randf()
+			var theta = randf() * 2.0 * PI
+			var phi = randf() * PI
+			var c = randf() * 3
+			if c <= 1:
+				p[0] = r * sin(phi) * cos(theta) + sim_radius
+				p[1] = r * sin(phi) * sin(theta)
+				p[2] = r * cos(phi)
+			if 1 < c and c <= 2:
+				p[0] = r * sin(phi) * cos(theta) - sim_radius / 2
+				p[1] = r * sin(phi) * sin(theta) + sim_radius / 2
+				p[2] = r * cos(phi)
+			if 2 < c:
+				p[0] = r * sin(phi) * cos(theta) - sim_radius / 2
+				p[1] = r * sin(phi) * sin(theta) - sim_radius / 2
+				p[2] = r * cos(phi)
+		
 		var m = starting_mass
-		#var a = Vector3((randf() - 0.5) * 0.1, 0, (randf() - 0.5) * 0.1)
 		var a = [0.0, 0.0, 0.0]
+		
+		if accel_option == 1:
+			a[0] = randf() * 0.1
+			a[1] = randf() * 0.1
+			a[2] = randf() * 0.1
+		if accel_option == 2:
+			a[0] = randf()
+			a[1] = randf()
+			a[2] = randf()
 		
 		points.append(p)
 		masses.append(m)
